4057dacb4ced949db7b99601f720989d8cd0a91d
Eunice Baiden <ebaiden@techie.com>
2018-09-29 20:19:31 -0400

    sw.js added to root folder

--- sw.js
+++ sw.js
@@ -1,1 +1,54 @@
 console.log('Service Worker: Registered');
+
+//Event Listener 
+self.addEventListener('install', function(e) {
+	e.waitUntil(
+		caches.open('v1').then(function(cache){
+			return cache.addAll(cacheFiles);
+		})
+
+		);
+
+});
+
+const cacheFiles = [
+//'/', 
+'restaurant.html', 
+'css/styles.css', 
+'js/dbhelper.js',
+'js/main.js',
+'js/index.js',
+'js/restaurant_info.js', 
+'data/restaurantsljson',
+'img/1.jpg',
+'img/2.jpg',
+'img/3.jpg',
+'img/4.jpg',
+'img/5.jpg',
+'img/6.jpg',
+'img/7.jpg',
+'img/8.jpg',
+'img/9.jpg',
+'img/10.jpg'
+];
+
+/*const cacheFiles = [
+'/', 
+'/restaurant.html', 
+'/css/styles.css', 
+'/js/dbhelper.js',
+'/js/main.js',
+'/js/index.js',
+'js/restaurant_info.js', 
+'/data/restaurantsljson',
+'/img/1.jpg',
+'/img/2.jpg',
+'/img/3.jpg',
+'/img/4.jpg',
+'/img/5.jpg',
+'/img/6.jpg',
+'/img/7.jpg',
+'/img/8.jpg',
+'/img/9.jpg',
+'/img/10.jpg'
+];/*
