3f68145859eaee6bdc47c1b787812b9fa1327069
Eunice Baiden <ebaiden@techie.com>
2018-09-30 05:36:58 -0400

    charset setting changed in the head

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ self.addEventListener('install', function(e) {
 });
 
 const cacheFiles = [
-//'/', 
+
 'restaurant.html', 
 'css/styles.css', 
 'js/dbhelper.js',
@@ -32,23 +32,26 @@ const cacheFiles = [
 'img/10.jpg'
 ];
 
-/*const cacheFiles = [
-'/', 
-'/restaurant.html', 
-'/css/styles.css', 
-'/js/dbhelper.js',
-'/js/main.js',
-'/js/index.js',
-'js/restaurant_info.js', 
-'/data/restaurantsljson',
-'/img/1.jpg',
-'/img/2.jpg',
-'/img/3.jpg',
-'/img/4.jpg',
-'/img/5.jpg',
-'/img/6.jpg',
-'/img/7.jpg',
-'/img/8.jpg',
-'/img/9.jpg',
-'/img/10.jpg'
-];/*
+self.addEventListener('fetch', function(e) {
+	e.respondWith(
+		caches.match(e.request).then(function(response) {
+			if(response) {
+				console.log('Found', e.request, 'in cache');
+				return response;
+			}
+			else {
+				console.log('Could not find', e.request, 'in cache, FETCHING');
+				return fetch(e.request)
+				.then(function(response) {
+					caches.open('v1').then(function(cache) {
+						cache.put(e.request, response);
+					})
+					return response;
+				})
+				.catch(function(err) {
+					console.log(err);
+				});
+			}
+		})
+		);
+});
